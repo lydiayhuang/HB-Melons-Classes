dec75849f2ac8cf8292accb7327d3f2d7a1bec0c
Hackbright Student <no-reply@hackbrightacademy.com>
2017-03-02 17:55:28 -0800

    added Christmas melons base price increase and account for orders less than 10.

--- melons.py
+++ melons.py
@@ -25,14 +25,19 @@ class DomesticMelonOrder(AbstractMelonOrder):
         """Initialize melon order attributes"""
         super(DomesticMelonOrder, selford).__init__(species, qty, 
                                             order_type = "domestic",
-                                            tax = 0.08)# check tax
+                                            tax = 0.08)
 
     def get_total(self):
         """Calculate price."""
 
-        base_price = 5
-        total = (1 + self.tax) * self.qty * base_price
-        return total
+        if self.species == 'Christmas':
+            base_price = 5*1.5
+            total = (1 + self.tax) * self.qty * base_price
+            return total
+        else:    
+            base_price = 5
+            total = (1 + self.tax) * self.qty * base_price
+            return total
 
     def mark_shipped(self):
         """Set shipped to true."""
@@ -41,30 +46,40 @@ class DomesticMelonOrder(AbstractMelonOrder):
 
 
 
-# class InternationalMelonOrder(AbstractMelonOrder):
-#     """An international (non-US) melon order."""
+class InternationalMelonOrder(AbstractMelonOrder):
+    """An international (non-US) melon order."""
 
-#     def __init__(self, species, qty):
-#         """Initialize melon order attributes"""
-#         super(InternationalMelonOrder, self).__init__(species, qty, "international")
+    def __init__(self, species, qty, country_code):
+        """Initialize melon order attributes"""
+        super(InternationalMelonOrder, self).__init__(species, qty, order_type = "international", tax = 0.17)
 
-#         self.country_code = country_code
+        self.country_code = country_code
 
 
 
-#     def get_total(self):
-#         """Calculate price."""
+    def get_total(self):
+        """Calculate price."""
 
-#         base_price = 5
-#         total = (1 + self.tax) * self.qty * base_price
-#         return total
+        if self.species == 'Christmas':
+            base_price = 5*1.5
+            total = (1 + self.tax) * self.qty * base_price
+            return total
+        else:    
+            base_price = 5
+            total = (1 + self.tax) * self.qty * base_price
+            return total
 
-#     def mark_shipped(self):
-#         """Set shipped to true."""
+        if self.qty < 10:
+            return total + 3
+        else:
+            return total
 
-#         self.shipped = True
+    def mark_shipped(self):
+        """Set shipped to true."""
 
-#     def get_country_code(self):
-#         """Return the country code."""
+        self.shipped = True
 
-#         return self.country_code
+    def get_country_code(self):
+        """Return the country code."""
+
+        return self.country_code
